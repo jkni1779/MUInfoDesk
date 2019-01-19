3d7c08c9bd1964f9bf4eba852e9c946a5e594082
Heisler <memu_nheisler@iastate.edu>
2019-01-19 09:42:58 -0600

    updated database_view model to add functionality for all remaining db tables

--- application/models/Database_view.php
+++ application/models/Database_view.php
@@ -21,11 +21,10 @@ class Database_view extends CI_Model
 		{
 			return false;
 		}
-
-
 	}
 
-
+	//the delete functions probably need fixed, I am probably not always working with "UID" table.
+	//need to better understand how the deleting in this api works
 
 	function fetch_identity()
 	{
@@ -70,6 +69,72 @@ class Database_view extends CI_Model
 	function delete_data_key_lists($id){
 		$this->db->where("UID", $id);
 		$this->db->delete("available_inventory");
+	}
+
+	function fetch_authorized_keys()
+	{
+		$query = $this->db->get("authorized_keys");
+		return $query;
+	}
+
+	function delete_data_authorized_keys($id){
+		$this->db->where("UID", $id);
+		$this->db->delete("authorized_keys");
+	}
+
+	function fetch_list_editors()
+	{
+		$query = $this->db->get("list_editors");
+		return $query;
+	}
+
+	function delete_data_list_editors($id){
+		$this->db->where("UID", $id);
+		$this->db->delete("list_editors");
+	}
+
+	function fetch_presets()
+	{
+		$query = $this->db->get("presets");
+		return $query;
+	}
+
+	function delete_data_presets($id){
+		$this->db->where("UID", $id);
+		$this->db->delete("presets");
+	}
+
+	function fetch_preset_items()
+	{
+		$query = $this->db->get("preset_items");
+		return $query;
+	}
+
+	function delete_data_preset_items($id){
+		$this->db->where("UID", $id);
+		$this->db->delete("preset_items");
+	}
+
+	function fetch_authorized_presets()
+	{
+		$query = $this->db->get("authorized_presets");
+		return $query;
+	}
+
+	function delete_data_authorized_presets($id){
+		$this->db->where("UID", $id);
+		$this->db->delete("authorized_presets");
+	}
+
+	function fetch_item_types()
+	{
+		$query = $this->db->get("item_types");
+		return $query;
+	}
+
+	function delete_data_item_types($id){
+		$this->db->where("UID", $id);
+		$this->db->delete("item_types");
 	}
 }
 ?>
